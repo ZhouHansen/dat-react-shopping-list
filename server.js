54d284bdbe18d537a1f1a94d20ed59ac59dba2fb
zhouhancheng <z308114274@gmail.com>
2019-10-07 13:14:07 +0800

    Update server.js

--- server.js
+++ server.js
@@ -9,14 +9,14 @@ expressWebSocket(app);
 
 cleanup();
 
-app.use(express.static("./build"));
+app.use(express.static("../build"));
 
 app.ws("/archive/:key", cb);
 
 // for client side render
 // e.g. refresh http://youserver/somelink without it will return 404
 app.get("*", (_req, res) => {
-  res.sendFile(path.join(__dirname + "/build/index.html"));
+  res.sendFile(path.join(__dirname, "../build/index.html"));
 });
 
 app.listen(3000, () => console.log(`Example app listening on port 3000!`));
